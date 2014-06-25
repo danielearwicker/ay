26df09682b12a6d07be462f38c3ccb91500e7cd8
Daniel Earwicker <dan@earwicker.com>
2014-06-25 21:17:26 +0100

    FIRST

---
+++ index.js
@@ -0,0 +1,110 @@
+var prototype = {
+    map: function(gen /*, thisArg */) {
+        var self = this;
+        return ay(function*() {            
+            var ar = yield self.generate(), 
+                len = ar.length >>> 0, 
+                result = new Array(len),
+                thisArg = arguments.length >= 2 ? arguments[1] : void 0;
+            for (var i = 0; i < len; i++) {
+                if (i in ar) {
+                    result[i] = yield gen.call(thisArg, ar[i], i, ar);
+                }
+            }
+            return result;
+        })
+    },
+    filter: function(gen /*, thisArg */) {
+        var self = this;
+        return ay(function*() {
+            var ar = yield self.generate(),
+                len = ar.length >>> 0, 
+                result = [],        
+                thisArg = arguments.length >= 2 ? arguments[1] : void 0;
+            for (var i = 0; i < len; i++) {
+                if (i in ar && (yield gen.call(thisArg, ar[i], i, ar))) {
+                    result.push(ar[i]);
+                }
+            }
+            return result;
+        });
+    },
+    reduce: function(gen, init) {
+        var self = this;
+        return ay(function*() {
+            var ar = yield self.generate(), len = ar.length >>> 0, k = 0, value;
+            if (arguments.length >= 2) {
+                value = arguments[1];
+            } else {
+                while (k < len && !k in ar) k++;
+                if (k >= len) {
+                    throw new TypeError('Reduce of empty array with no initial value');
+                }
+                value = ar[k++];
+            }
+            for (; k < len; k++) {
+                if (k in ar) {
+                    value = yield gen(value, ar[k], k, ar);
+                }
+            }
+            return value;
+        });
+    },
+    reduceRight: function(gen, init) {
+        var self = this;
+        return ay(function*() {
+            var ar = yield self.generate(), len = ar.length >>> 0, k = len - 1, value;
+            if (arguments.length >= 2) {
+                value = arguments[1];
+            } else {
+                while (k >= 0 && !k in t) k--;
+                if (k < 0) {
+                    throw new TypeError('Reduce of empty array with no initial value');
+                }
+                value = ar[k--];
+            }
+            for (; k >= 0; k--) {
+                if (k in ar) {
+                    value = yield gen(value, ar[k], k, ar);
+                }
+            }
+            return value;
+        });
+    },
+    every: function(gen /*, thisArg */) {
+        var self = this;
+        return ay(function*() {
+            var ar = yield self.generate(), len = ar.length >>> 0,
+                thisArg = arguments.length >= 2 ? arguments[1] : void 0;
+            for (var i = 0; i < len; i++) {
+                if ((i in ar) && !(yield gen.call(thisArg, ar[i], i, ar))) {
+                    return false;
+                }
+            }
+            return true;
+       });
+    },
+    some: function(gen /*, thisArg */) {
+        var self = this;
+        return ay(function*() {
+            var ar = yield self.generate(), len = ar.length >>> 0,
+                thisArg = arguments.length >= 2 ? arguments[1] : void 0;
+            for (var i = 0; i < len; i++) {
+                if ((i in ar) && (yield gen.call(thisArg, ar[i], i, ar))) {
+                    return true;
+                }
+            }
+            return false;
+        });
+    }
+};
+
+var ay = module.exports = function(g) {
+    if (typeof g !== 'function') {
+        g = g === void 0 ? [] : Array.isArray(g) ? g : [g];    
+        return ay(function* () { return g; });
+    }
+    return Object.create(prototype, {
+        generate: { value: g }
+    });
+};
---
+++ test/test.js
@@ -0,0 +1,49 @@
+var assert = require('assert');
+var co = require('co');
+
+var ay = require('../index.js');
+
+function sleep(ms) {
+    return function(done) {
+        setTimeout(function() { done(); }, ms);
+    };
+}
+
+describe('ay', function() {
+    describe('chaining', function() {
+        it('should work the same as ordinary array methods', co(function* () {
+    
+            var nums = [1, 2, 3, 4, 5];
+
+            var result1 = nums
+                .map(function (i) {
+                    return i * 2;
+                })
+                .filter(function (i) {
+                    return i > 2;
+                })
+                .reduce(function (a, b) {
+                    return a + b;
+                });
+        
+            assert.equal(28, result1);
+                    
+            var result2 = yield ay(nums)
+                .map(function* (i) {
+                    yield sleep(10);
+                    return i * 2;
+                })
+                .filter(function* (i) {
+                    yield sleep(10);
+                    return i > 2;
+                })
+                .reduce(function* (a, b) {
+                    yield sleep(10);
+                    return a + b;
+                })
+                .generate();
+            
+            assert.equal(result2, result1);
+        }));
+    });
+});
